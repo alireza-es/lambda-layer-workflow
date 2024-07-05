7357b3cf002353eaf61e702dda84e2f1656f75dc
Alireza Esmaeili <alireza.es@gmail.com>
2024-07-04 19:56:55 -0600

    Update org name

--- example/providers.tf
+++ example/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.0"
   cloud {
     hostname     = "app.terraform.io"
-    organization = "spike-spiegel"
+    organization = "honeybee-logic"
     workspaces {
       name = "lambda-layers-workflow"
     }
